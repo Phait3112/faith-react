480751a2a9743cf2efc7136ce4adff15345bfeae
Phait3112 <faithvicosamor@gmail.com>
2026-03-09 05:53:09 -0700

    added hii

--- src/Education.jsx
+++ src/Education.jsx
@@ -4,7 +4,7 @@ function Education({text1}) {
   return (
     <div className=' h-[150px] w-[390px] text-white'>
         <p className=" mb-[10px] text-[xx-large]">{text1}</p>
-        <p className=" ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia optio pariatur itaque deleniti non adipisci nemo dolor qui modi quod!</p>
+        <p className=" ">Lorem ipsum dolor sit,jhii amet consectetur adipisicing elit. Mollitia optio pariatur itaque deleniti non adipisci nemo dolor qui modi quod!</p>
     </div>
   )
 }
